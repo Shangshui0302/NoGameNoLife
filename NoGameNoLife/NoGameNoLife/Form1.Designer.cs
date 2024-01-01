d057d1620ebbe10294cde4ef16912698b79551d5
Shangshui0302 <149566800+Shangshui0302@users.noreply.github.com>
2024-01-01 18:54:52 +0800

    Add files via upload
    The all.

---
+++ NoGameNoLife/NoGameNoLife/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace NoGameNoLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GameStart = new System.Windows.Forms.Button();
+            this.GameStop = new System.Windows.Forms.Button();
+            this.CLR = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.random = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.EditStart = new System.Windows.Forms.Button();
+            this.EditEnd = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.probability = new System.Windows.Forms.ComboBox();
+            this.text_probability = new System.Windows.Forms.Label();
+            this.text_size = new System.Windows.Forms.Label();
+            this.cellsize = new System.Windows.Forms.ComboBox();
+            this.Rules = new System.Windows.Forms.Label();
+            this.speed = new System.Windows.Forms.TrackBar();
+            this.text_speed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.speed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameStart
+            // 
+            this.GameStart.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.GameStart.Location = new System.Drawing.Point(622, 33);
+            this.GameStart.Name = "GameStart";
+            this.GameStart.Size = new System.Drawing.Size(75, 23);
+            this.GameStart.TabIndex = 0;
+            this.GameStart.Text = "推演";
+            this.GameStart.UseVisualStyleBackColor = true;
+            this.GameStart.Click += new System.EventHandler(this.GameStart_Click);
+            // 
+            // GameStop
+            // 
+            this.GameStop.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.GameStop.Location = new System.Drawing.Point(703, 33);
+            this.GameStop.Name = "GameStop";
+            this.GameStop.Size = new System.Drawing.Size(75, 23);
+            this.GameStop.TabIndex = 1;
+            this.GameStop.Text = "停止";
+            this.GameStop.UseVisualStyleBackColor = true;
+            this.GameStop.Click += new System.EventHandler(this.GameStop_Click);
+            // 
+            // CLR
+            // 
+            this.CLR.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.CLR.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.CLR.Location = new System.Drawing.Point(622, 62);
+            this.CLR.Name = "CLR";
+            this.CLR.Size = new System.Drawing.Size(75, 23);
+            this.CLR.TabIndex = 2;
+            this.CLR.Text = "初始化";
+            this.CLR.UseVisualStyleBackColor = true;
+            this.CLR.Click += new System.EventHandler(this.CLR_Click);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(34, 34);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(558, 377);
+            this.panel.TabIndex = 3;
+            this.panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_MouseClick);
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_MouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_MouseUp);
+            // 
+            // random
+            // 
+            this.random.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.random.Location = new System.Drawing.Point(703, 62);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(75, 23);
+            this.random.TabIndex = 4;
+            this.random.Text = "随机生成";
+            this.random.UseVisualStyleBackColor = true;
+            this.random.Click += new System.EventHandler(this.random_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // EditStart
+            // 
+            this.EditStart.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.EditStart.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.EditStart.Location = new System.Drawing.Point(622, 143);
+            this.EditStart.Name = "EditStart";
+            this.EditStart.Size = new System.Drawing.Size(75, 23);
+            this.EditStart.TabIndex = 5;
+            this.EditStart.Text = "开始编辑";
+            this.EditStart.UseVisualStyleBackColor = true;
+            this.EditStart.Click += new System.EventHandler(this.EditStart_Click);
+            // 
+            // EditEnd
+            // 
+            this.EditEnd.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.EditEnd.Location = new System.Drawing.Point(703, 143);
+            this.EditEnd.Name = "EditEnd";
+            this.EditEnd.Size = new System.Drawing.Size(75, 23);
+            this.EditEnd.TabIndex = 6;
+            this.EditEnd.Text = "结束编辑";
+            this.EditEnd.UseVisualStyleBackColor = true;
+            this.EditEnd.Click += new System.EventHandler(this.EditEnd_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label.Location = new System.Drawing.Point(32, 414);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(73, 18);
+            this.label.TabIndex = 7;
+            this.label.Text = "存活细胞数";
+            // 
+            // probability
+            // 
+            this.probability.FormattingEnabled = true;
+            this.probability.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.probability.Location = new System.Drawing.Point(703, 91);
+            this.probability.Name = "probability";
+            this.probability.Size = new System.Drawing.Size(75, 20);
+            this.probability.TabIndex = 8;
+            this.probability.SelectedIndexChanged += new System.EventHandler(this.probability_SelectedIndexChanged);
+            // 
+            // text_probability
+            // 
+            this.text_probability.AutoSize = true;
+            this.text_probability.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.text_probability.Location = new System.Drawing.Point(628, 91);
+            this.text_probability.Name = "text_probability";
+            this.text_probability.Size = new System.Drawing.Size(60, 18);
+            this.text_probability.TabIndex = 9;
+            this.text_probability.Text = "生成概率";
+            // 
+            // text_size
+            // 
+            this.text_size.AutoSize = true;
+            this.text_size.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.text_size.Location = new System.Drawing.Point(628, 117);
+            this.text_size.Name = "text_size";
+            this.text_size.Size = new System.Drawing.Size(60, 18);
+            this.text_size.TabIndex = 11;
+            this.text_size.Text = "细胞尺寸";
+            // 
+            // cellsize
+            // 
+            this.cellsize.FormattingEnabled = true;
+            this.cellsize.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.cellsize.Location = new System.Drawing.Point(703, 117);
+            this.cellsize.Name = "cellsize";
+            this.cellsize.Size = new System.Drawing.Size(75, 20);
+            this.cellsize.TabIndex = 10;
+            this.cellsize.SelectedIndexChanged += new System.EventHandler(this.cellsize_SelectedIndexChanged);
+            // 
+            // Rules
+            // 
+            this.Rules.Font = new System.Drawing.Font("HarmonyOS Sans SC", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Rules.Location = new System.Drawing.Point(610, 169);
+            this.Rules.Name = "Rules";
+            this.Rules.Size = new System.Drawing.Size(178, 257);
+            this.Rules.TabIndex = 12;
+            this.Rules.Text = resources.GetString("Rules.Text");
+            // 
+            // speed
+            // 
+            this.speed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.speed.Location = new System.Drawing.Point(358, 414);
+            this.speed.Maximum = 1000;
+            this.speed.Minimum = 1;
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(234, 45);
+            this.speed.SmallChange = 10;
+            this.speed.TabIndex = 13;
+            this.speed.Value = 500;
+            this.speed.Scroll += new System.EventHandler(this.speed_Scroll);
+            // 
+            // text_speed
+            // 
+            this.text_speed.AutoSize = true;
+            this.text_speed.Font = new System.Drawing.Font("HarmonyOS Sans SC", 9.75F, System.Drawing.FontStyle.Bold);
+            this.text_speed.Location = new System.Drawing.Point(292, 423);
+            this.text_speed.Name = "text_speed";
+            this.text_speed.Size = new System.Drawing.Size(60, 18);
+            this.text_speed.TabIndex = 14;
+            this.text_speed.Text = "推演速度";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_speed);
+            this.Controls.Add(this.speed);
+            this.Controls.Add(this.Rules);
+            this.Controls.Add(this.text_size);
+            this.Controls.Add(this.cellsize);
+            this.Controls.Add(this.text_probability);
+            this.Controls.Add(this.probability);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.EditEnd);
+            this.Controls.Add(this.EditStart);
+            this.Controls.Add(this.random);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.CLR);
+            this.Controls.Add(this.GameStop);
+            this.Controls.Add(this.GameStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NoGameNoLife【康威生命游戏】";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.speed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GameStart;
+        private System.Windows.Forms.Button GameStop;
+        private System.Windows.Forms.Button CLR;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button random;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button EditStart;
+        private System.Windows.Forms.Button EditEnd;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ComboBox probability;
+        private System.Windows.Forms.Label text_probability;
+        private System.Windows.Forms.Label text_size;
+        private System.Windows.Forms.ComboBox cellsize;
+        private System.Windows.Forms.Label Rules;
+        private System.Windows.Forms.TrackBar speed;
+        private System.Windows.Forms.Label text_speed;
+    }
+}
+
---
+++ NoGameNoLife/NoGameNoLife/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace NoGameNoLife
+{
+    public partial class Form1 : Form
+    {
+        #region 参数
+        //全局
+        bool[,] map;//存储整个世界地图
+        int maplength = 2500;//世界地图的长度
+        int interval = 500;//时钟刷新间隔
+        int length = 750;//画布长度
+        int size = 5;//单个细胞大小
+        int count;//每行每列细胞数量
+        int x = 1250;//地图中心x坐标
+        int y = 1250;//地图中心y坐标
+        int percent = 5;//生成细胞的概率
+        //状态
+        bool edit = false;//是否编辑状态
+        bool slidepaint = false;//移动绘制
+        int gen = 0;//繁衍代数
+        #endregion
+        #region 初始化
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            ForBeginner();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //添加概率调整项
+            probability.Items.Clear();
+            for (int i = 0; i <= 9; i++) probability.Items.Add(i.ToString());
+            probability.SelectedIndex = 6;
+            //添加细胞尺寸调整项
+            cellsize.Items.Clear();
+            string[] sizelist = { "1", "5", "10", "15", "20", "25", "30" };
+            cellsize.Items.AddRange(sizelist);
+            cellsize.SelectedIndex = 1;
+            //速度条调整
+            speed.Maximum = 1000;
+            speed.Minimum = 1;
+            speed.SmallChange = 10;
+            speed.TabIndex = 13;
+            speed.Value = 500;
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //创建空白画布
+            draw();
+        }
+        public void ForBeginner()//游戏介绍
+        {
+            MessageBox.Show("生命游戏是英国数学家约翰·何顿·康威在1970年发明的细胞自动机。\n 它最初于1970年10月在《科学美国人》杂志上发布。","需要知道的规则",MessageBoxButtons.OK);
+            MessageBox.Show("作为一个模拟仿真游戏你有几个要记住的规则。", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("每个细胞有两种状态 - 存活或死亡，每个细胞与以自身为中心的周围八格细胞产生互动（如图，黑色为存活，白色为死亡）", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("当前细胞为存活状态时，当周围的存活细胞低于2个时（不包含2个），该细胞变成死亡状态。（模拟生命数量稀少）", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("当前细胞为存活状态时，当周围有2个或3个存活细胞时，该细胞保持原样。", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("当前细胞为存活状态时，当周围有超过3个存活细胞时，该细胞变成死亡状态。（模拟生命数量过多）", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("当前细胞为死亡状态时，当周围有3个存活细胞时，该细胞变成存活状态。（模拟繁殖）", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("记住了吗？没记住也没关系。\n我会把它张贴在游戏内。", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("总之，现在开始创造你的第一个生命吧！", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("哦对了,还有一件事", "需要知道的规则", MessageBoxButtons.OK);
+            MessageBox.Show("\"我是上帝,你也是\"\n----雷善水", "需要知道的规则", MessageBoxButtons.OK);
+        }
+        public void Init()//初始化
+        {
+            //参数初始化
+            timer.Interval = interval;//初始化时钟间隔
+            count = length / size;//计算细胞数量
+            map = new bool[maplength, maplength];//初始化世界地图
+            int sumcount = 0;
+            //控件初始化
+            GameStart.Enabled = true;//推演
+            GameStop.Enabled = false;//停止推演
+            EditStart.Enabled = true;//开始编辑
+            EditEnd.Enabled = false;//停止编辑
+            label.Text = $"整体存活细胞:{sumcount}";//细胞数
+            //地图初始化
+            for (int i = 0; i < maplength; i++)
+                for (int j = 0; j < maplength; j++)
+                    map[i, j] = false;
+        }
+        #endregion
+        #region 核心功能函数_衍化
+        public void draw()//绘制
+        {
+            Bitmap myImage = new Bitmap(length, length);//建立像素画布
+            Graphics g = Graphics.FromImage(myImage);//建立graphics类，从而对map进行更改
+            g.Clear(Color.White);//清空
+            int cellcount = 0;//活细胞数
+            lock (map)//统一刷新
+            {
+                for (int i = x - count / 2; i < x + count / 2; i++)//绘制
+                {
+                    for (int j = y - count / 2; j < y + count / 2; j++)
+                    {
+                        if (map[i, j])
+                        {
+                            g.FillRectangle(Brushes.Black, (i - x + count / 2) * size, (j - y + count / 2) * size, size, size);
+                            cellcount++;//统计细胞数
+                        }
+                    }
+                }
+            }
+            g.Dispose();//释放graphic类，节约内存，聊胜于无
+            Graphics g2 = this.panel.CreateGraphics();//绘制到panel上
+            g2.DrawImage(myImage, 0, 0);
+
+        }
+        public void nextgen()//繁衍函数
+        {
+            int[,] count = new int[maplength + 2, maplength + 2];//单个细胞周边细胞数计数
+            int sumcount = 0;
+            for (int i = 0; i < maplength; i++)//遍历
+            {
+                for (int j = 0; j < maplength; j++)
+                {
+                    if (map[i, j])//若存活，周围的细胞的“周围细胞数”均增一
+                    {
+                        count[i, j]++; count[i, j + 1]++; count[i, j + 2]++;
+                        count[i + 1, j]++; count[i + 1, j + 2]++;
+                        count[i + 2, j]++; count[i + 2, j + 1]++; count[i + 2, j + 2]++;
+                        sumcount++;
+                    }
+                }
+            }
+            for (int i = 0; i < maplength; i++)//count[1,1]对应map[0,0]
+            {
+                for (int j = 0; j < maplength; j++)
+                {
+                    switch (count[i + 1, j + 1])
+                    {
+                        case 0:
+                        case 1:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                            map[i, j] = false; break;//多了或者少了死去
+                        case 3: map[i, j] = true; break;//繁殖或者保持，case2即default保持
+                    }
+                }
+            }
+            gen++;
+            label.Text = $"整体存活细胞:{sumcount}";
+        }//tips：时间度太复杂了，遍历两次，考虑优化算法
+        public int Randomize()//随机化世界
+        {
+            int sumcount = 0;//总体细胞数
+            Random rd = new Random();//初始化随机数生成器
+            for (int i = 0; i < maplength; i++)//初始化地图
+            {
+                for (int j = 0; j < maplength; j++)
+                {
+                    map[i, j] = rd.Next(0, 10) < percent;//概率生成：生成10个数，小于percent返回true，否则false，percent不可大于9
+                    if (map[i, j])
+                        sumcount++;
+                }
+            }
+            return sumcount;
+        }
+        #endregion
+        #region 编辑模式
+        private void EditStart_Click(object sender, EventArgs e)
+        {
+            edit = true;//正在编辑
+            timer.Enabled = false;//停止推演
+            GameStart.Enabled = false;//失能推演
+            GameStop.Enabled = false;//失能停止推演
+            EditStart.Enabled = false;//失能编辑
+            EditEnd.Enabled = true;//使能停止编辑
+            probability.Enabled = false;
+            cellsize.Enabled = false;
+        }
+        private void EditEnd_Click(object sender, EventArgs e)
+        {
+            edit = false;//停止编辑
+            EditStart.Enabled = true;//使能开始编辑
+            EditEnd.Enabled = false;//失能停止编辑
+            GameStart.Enabled = true;//使能推演
+            GameStop.Enabled = true;//使能停止推演
+            probability.Enabled = true;
+            cellsize.Enabled = true;
+            draw();
+        }
+        private void panel_MouseClick(object sender, MouseEventArgs e)//点击修改
+        {
+            if(edit)
+            {
+                int X = e.X / size + x - count / 2;
+                int Y = e.Y / size + y - count / 2;
+                map[X, Y] = !map[X, Y];//翻转
+                Graphics g = this.panel.CreateGraphics();
+                g.FillRectangle((map[X, Y] ? Brushes.Black : Brushes.White), (e.X / size) * size, (e.Y / size) * size, size, size);
+            }
+        }
+        private void panel_MouseMove(object sender, MouseEventArgs e)//滑动修改
+        {
+            if (edit)//正在编辑模式
+            {
+                if (slidepaint)//进入图画模式
+                {
+                    int X = e.X / size + x - count / 2;
+                    int Y = e.Y / size + y - count / 2;
+                    map[X, Y] = true;//一概设为真
+                    Graphics g = this.panel.CreateGraphics();
+                    g.FillRectangle((map[X, Y] ? Brushes.Black : Brushes.White), (e.X / size) * size, (e.Y / size) * size, size, size);//内部
+                }
+            }
+        }
+        private void panel_MouseDown(object sender, MouseEventArgs e)//开始编辑模式
+        {
+            if (edit)slidepaint = true;
+        }
+        private void panel_MouseUp(object sender, MouseEventArgs e)//结束编辑模式
+        {
+            if (edit)slidepaint = false;
+        }
+        //没搞懂eventhandler什么意思，虽然能跑。
+        //大概是在事件栈或者队列里添加一个事件，那原先的事件或者说栈底队列头的事件是什么？就是空吗？会不会占内存？2 
+        //可以从控件的行为窗口添加事件，会自动在designer生成eventhandler，也可以写好后在designer绑定
+        #endregion
+        #region 控件事件
+        private void GameStart_Click(object sender, EventArgs e)
+        {
+            GameStart.Enabled = false;
+            GameStop.Enabled = true;
+            EditStart.Enabled = false;
+            EditEnd.Enabled = false;
+            random.Enabled = false;
+            CLR.Enabled = false;
+            probability.Enabled = false;
+            cellsize.Enabled = false;
+            timer.Enabled = true;
+            draw();
+        }
+
+        private void CLR_Click(object sender, EventArgs e)
+        {
+            gen = 0;
+            for (int i = 0; i < maplength; i++)
+                for (int j = 0; j < maplength; j++)
+                    map[i, j] = false;
+            label.Text = $"整体存活细胞:0";
+            draw();
+        }
+
+        private void GameStop_Click(object sender, EventArgs e)
+        {
+            GameStart.Enabled = true;
+            EditStart.Enabled = true;
+            EditEnd.Enabled = true;
+            random.Enabled = true;
+            CLR.Enabled = true;
+            probability.Enabled = true;
+            cellsize.Enabled = true;
+            GameStop.Enabled = false;
+            timer.Enabled = false;
+            draw();
+        }
+
+        private void random_Click(object sender, EventArgs e)
+        {
+            gen = 0;
+            int sumcount = Randomize();//初始化世界
+            label.Text = $"整体存活细胞:{sumcount}";//细胞数
+            draw();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            nextgen();
+            draw();
+        }
+
+        private void cellsize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            size = Convert.ToInt32(cellsize.SelectedItem);
+        }
+
+        private void probability_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            percent = Convert.ToInt32(probability.SelectedItem);
+        }
+
+        private void speed_Scroll(object sender, EventArgs e)
+        {
+            interval = 1001 - speed.Value;
+            timer.Interval = interval;
+        }
+        #endregion
+    }
+}
